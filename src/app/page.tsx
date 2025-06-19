00c1cbb8478e592ccf302187a5a4b3016ae82d8c
jeekwar <kurniawan.vendor.pasti@pegadaian.co.id>
2025-06-19 15:16:45 +0700

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+export default function Page() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>Welcome to App Router!</h1>
+      <p>This uses React Server Components by default.</p>
+    </div>
+  )
+}
