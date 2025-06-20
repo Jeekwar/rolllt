b5f707cfdbe37dc5c1455392693ac2c9ef0d52e5
jeekwar <kurniawan.vendor.pasti@pegadaian.co.id>
2025-06-20 15:59:02 +0700

    chore: remove chakra ui and add shadcn

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
+  images: {
+    domains: ['image.tmdb.org'],
   },
   // devIndicators: false
 };
---
+++ src/services/apiConfig.ts
@@ -0,0 +1,9 @@
+const apiUrls = {
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    imageBaseUrl: process.env.NEXT_PUBLIC_BASE_IMAGE_URL,
+    discoverMovies: "/discover/movie",
+    movie: "/movie"
+}
+
+
+export default apiUrls
