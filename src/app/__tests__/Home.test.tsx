dffa0fdbd5796885de32dd23579f105244be3b23
jeekwar <kurniawan.vendor.pasti@pegadaian.co.id>
2025-06-21 14:06:46 +0700

    test: add unit tets Home page

---
+++ src/app/__tests__/Home.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react';
+import Home from '@/app/page';
+
+jest.mock('@/components/organisms/Header', () => () => <div data-testid="header">Mock Header</div>);
+
+jest.mock('@/components/templates/List', () => () => <div data-testid="list">Mock List</div>)
+
+
+describe('Home Page', () => {
+
+  it('Header and List sholud be rendered', () => {
+    render(<Home/>)
+    expect(screen.getByTestId('header')).toBeInTheDocument()
+    expect(screen.getByTestId('list')).toBeInTheDocument()
+  })
+
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+
 import Header from "@/components/organisms/Header";
 import ListMovie from "@/components/templates/List";
 
